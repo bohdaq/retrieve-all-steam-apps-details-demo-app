52c2142f95b8ea6f19d3908cc42e86608643d89c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-23 20:07:46 +0300

    fine tuning

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ do
 	killall retrieve-all-steam-apps-details-demo-app >> out.log &
 	cargo run >> out.log &
 	echo "started app with PID $!, output redirected to out.log"
-	echo "instance will be restarted in 300s"
+	echo "instance will be restarted in 900s"
 	echo "end of iteration $i"
 	let i++
-	sleep 300
+	sleep 900
 done
