924a8278fe25b7bbe08404600fec7798edd2eb7a
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-23 14:58:41 +0300

    updated run script

--- run.sh
+++ run.sh
@@ -3,11 +3,15 @@ PID=0
 
 while true
 do
-	echo "iteration $i"
-	pkill -9 $PID &
+	echo "\n\niteration $i"
+	sh stop-instance.sh
+	echo "killing previously started instance PID"
 	cargo run > out.log &
+	echo "started app with PID $!, output redirected to out.log"
 	PID=$!
-	let i++
+	echo "pkill -9 $PID">stop-instance.sh
+	echo "instance will be restarted in 600s"
 	echo "end of iteration $i"
+	let i++
 	sleep 600
 done
