f1caf6b5ce9b3de97dc648e443c31d3acce0387b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-24 15:57:30 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::{File, OpenOptions, read_to_string};
 use std::path::Path;
 use std::{fs, thread, time};
-use std::io::Write;
+use std::io::{Read, Write};
+use sha256::digest;
 
 // How to use: 1. First step is to import crate functions.
 use steam_webapi_rust_sdk::{get_app_list, get_app_details};
@@ -22,6 +23,7 @@ fn do_job() {
 
     println!("Getting list of already processed app ids. This may take a while...");
     let already_processed_app_id_list_path = [get_cache_dir_path(), "/".to_string(), "processed_app_id_list.json".to_string()].join("");
+    let already_processed_app_id_list_path_sha_256 = [get_cache_dir_path(), "/".to_string(), "processed_app_id_list.json.sha256".to_string()].join("");
     let backup_already_processed_app_id_list_path = [get_cache_dir_path(), "/".to_string(), "backup_processed_app_id_list.json".to_string()].join("");
     let file_exists = Path::new(already_processed_app_id_list_path.as_str()).is_file();
     if file_exists {
@@ -30,6 +32,41 @@ fn do_job() {
             let boxed_processed_app_id_list = serde_json::from_str(serialized_string.as_str());
             if boxed_processed_app_id_list.is_ok() {
                 processed_app_id_list = boxed_processed_app_id_list.unwrap();
+
+
+                //Verification
+                let list_as_string: String = format!("{:?}", &processed_app_id_list);
+                let list_as_u8 : &[u8] = list_as_string.as_bytes();
+                let sha_256 = digest(list_as_u8);
+                println!("SHA256 deserialized list: {}", sha_256);
+
+                let mut file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .truncate(false)
+                    .open(&already_processed_app_id_list_path_sha_256)
+                    .unwrap();
+
+                let mut sha256_from_file: String = "".to_string();
+                let boxed_sha = file.read_to_string(&mut sha256_from_file);
+                if boxed_sha.is_ok() {
+                    println!("SHA256 from file: {}", sha256_from_file);
+                }
+
+                if sha_256 != sha256_from_file {
+                    println!("restoring from backup as SHAs do not match");
+                    let boxed_backup_restore = fs::copy(&backup_already_processed_app_id_list_path, &already_processed_app_id_list_path);
+                    if boxed_backup_restore.is_err() {
+                        println!("backup restore failed, exiting...");
+                        return;
+                    }
+                    //retry after backup restore
+                    do_job();
+                }
+
+
+
                 let boxed_backup = fs::copy(&already_processed_app_id_list_path, &backup_already_processed_app_id_list_path);
                 if boxed_backup.is_err() {
                     println!("backup creation failed, exiting...");
@@ -81,10 +118,26 @@ fn do_job() {
         println!("\n\n Iteration number: {} \n App List size:    {}  {}%  After filtering: {}", iteration_number, app_list_size, calculated_percentage, filtered_list_len);
         retrieve_detailed_app_info(app.appid);
         iteration_number = iteration_number + 1;
-        processed_app_id_list.push(app.appid);
+        &processed_app_id_list.push(app.appid);
 
         let serialized_list = serde_json::to_string(&processed_app_id_list).unwrap();
         file.write_all(serialized_list.as_ref()).unwrap();
+
+
+        //write sha256
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(false)
+            .open(&already_processed_app_id_list_path_sha_256)
+            .unwrap();
+
+        let list_as_string: String = format!("{:?}", &processed_app_id_list);
+        let list_as_u8 : &[u8] = list_as_string.as_bytes();
+        let sha256_from_list = digest(list_as_u8);
+        file.write_all(sha256_from_list.as_ref()).unwrap();
+        println!("SHA256 after write for the list of already processed app ids: {}", sha256_from_list);
     }
 }
 
