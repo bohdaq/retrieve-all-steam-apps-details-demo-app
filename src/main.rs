8856e6d9884b7bbbb0611882ac75809eab65a76f
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-27 01:00:14 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -257,8 +257,13 @@ fn get_steam_app_list() -> Vec<SteamApp> {
     app_list
 }
 
-fn setup_encryption() {
+fn setup_encryption() -> Result<(), String> {
     let boxed_passphrase = get_or_create_passphrase();
+    if boxed_passphrase.is_err() {
+        return Err(boxed_passphrase.err().unwrap());
+    }
+
+    Ok(())
 }
 
 fn get_or_create_passphrase() -> Result<String, String> {
@@ -266,7 +271,11 @@ fn get_or_create_passphrase() -> Result<String, String> {
 
     let does_passphrase_exist = does_file_exist(passphrase_path);
     return if does_passphrase_exist {
-        let passphrase = read_file(passphrase_path);
+        let boxed_read = read_file(passphrase_path);
+        if boxed_read.is_err() {
+            return Err(boxed_read.err().unwrap());
+        }
+        let passphrase = boxed_read.unwrap();
         Ok(passphrase)
     } else {
         let boxed_create = create_file(passphrase_path);
@@ -305,6 +314,7 @@ fn create_file(path: &str) -> Result<File, String>  {
         return Err(message)
     }
 
+    let file = boxed_file.unwrap();
     Ok(file)
 }
 
@@ -313,23 +323,28 @@ fn does_file_exist(path: &str) -> bool {
     file_exists
 }
 
-fn read_file(path: &str) -> String {
+fn read_file(path: &str) -> Result<String, String> {
     let mut file_contents : String = "".to_string();
-
-    let mut file = OpenOptions::new()
+    let boxed_open = OpenOptions::new()
         .read(true)
         .write(false)
         .create(false)
         .truncate(false)
-        .open(path)
-        .unwrap();
+        .open(path);
+    if boxed_open.is_err() {
+        let message = format!("unable to read from file: {}", boxed_open.err().unwrap());
+        return Err(message)
+    }
+
+    let mut file = boxed_open.unwrap();
 
     let boxed_read = file.read_to_string(&mut file_contents);
     if boxed_read.is_err() {
-        println!("unable to read from file: {}", boxed_read.err().unwrap());
+        let message = format!("unable to read from file: {}", boxed_read.err().unwrap());
+        return Err(message)
     }
 
-    file_contents
+    Ok(file_contents)
 }
 
 fn write_file(path: &str, file_content: &[u8]) -> Result<(), String> {
