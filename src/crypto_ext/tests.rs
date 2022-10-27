33820bd881b63afb32a07d6bd5b9f681d465d283
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-28 01:06:37 +0300

    wip

--- src/crypto_ext/tests.rs
+++ src/crypto_ext/tests.rs
@@ -2,7 +2,10 @@ use crate::crypto_ext::{decrypt, encrypt, setup_encryption};
 
 #[test]
 fn encryption() {
-    let params = setup_encryption(Some("/test/encryption_parameters/")).unwrap();
+    // path needs to be accessible by user with write permission for initial setup
+    let relative_path_to_working_directory_for_storing_encryption_parameters = "/test/encryption_parameters/";
+    // it will read encryption params like public, private keys and passphrase or create
+    let params = setup_encryption(Some(relative_path_to_working_directory_for_storing_encryption_parameters)).unwrap();
 
     //maximum 501 bytes at once to be encrypted
     let data = "Some random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomee123textSomee123textSomee123textSo";
